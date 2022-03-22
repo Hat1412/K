ecfcfab9f31b4fdd6cb2162908b8540635215939
Hat1412 <72010214+Hat1412@users.noreply.github.com>
2022-03-22 18:07:27 +0530

    Add files via upload

---
+++ idk.py
@@ -0,0 +1,42 @@
+from math import sqrt
+import matplotlib.pyplot as plt
+
+def isprime(num):
+    a=2
+    while a <= sqrt(num):
+        if num%a<1:
+            return False
+        
+        a=a+1
+    return num>1
+    
+def prime_series(n):
+    l = [i for i in range(n) if isprime(i)]
+    plt.scatter(l,[i for i in range(len(l))])
+
+def Fib(n):
+    l = [0,1]
+    for _ in range(n - 2):
+        c = l[-1] + l[-2]
+        l.append(c)
+        
+    plt.scatter(l,[i for i in range(len(l))])
+
+def AP(a,n,d):
+    l = []
+    for i in range(1,n + 1):
+        l.append(a + (i-1) * d)
+
+    plt.scatter(l,[i for i in range(len(l))])
+
+    return l
+
+def HS(l):
+    res = []
+    for i in l:
+        res.append(round(1/i,2))
+
+    return res
+
+print(HS(AP(1,25,2)))
+plt.show()
